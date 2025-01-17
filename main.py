6146ed66b4e748352c539d2a4cd003bac3499a57
Skryptor <20Dimon19997@gmail.com>
2025-01-17 19:07:31 +0300

    delete_and_search

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from os.path import curdir
+
 import psycopg2
 import configparser
 config = configparser.ConfigParser()
@@ -73,15 +75,6 @@ class UploadUser:
         """, (number, userid))
         conn.commit()
 
-class UpdateUser:
-
-    def __init__(self, first_names=None, last_names=None, emails=None, number=None, userid=None):
-        self.first_names = first_names
-        self.last_names = last_names
-        self.emails = emails
-        self.number = number
-        self.userid = userid
-
     def update_value_num(self, conn):
 
         if not all([self.number, self.userid]):
@@ -119,5 +112,67 @@ class UpdateUser:
         with conn.cursor() as cur:
             cur.execute( value,tuple(values))
         conn.commit()
+
+    def delete_value(self, conn, table_name):
+        if not self.userid:
+            raise ValueError('Необходимо указать ID пользователя.')
+
+        conditions = ["id = %s"]
+        values = [self.userid]
+
+        if table_name == "Client":
+            if self.first_name:
+                conditions.append("first_name = %s")
+                values.append(self.first_name)
+            if self.last_name:
+                conditions.append("last_name = %s")
+                values.append(self.last_name)
+            if self.email:
+                conditions.append("email = %s")
+                values.append(self.email)
+
+        elif table_name == "Numbers":
+            if self.number:
+                conditions.append("number = %s")
+                values.append(self.number)
+
+        else:
+            raise ValueError('Неправильное имя таблицы.')
+
+        query = f"DELETE FROM {table_name} WHERE {' AND '.join(conditions)}"
+
+        with conn.cursor() as cur:
+            cur.execute(query, tuple(values))
+        conn.commit()
+
+    def search_user(self, conn, table_name, first_name=None, last_name=None, email=None, number=None):
+        conditions = []
+        values = []
+
+        if first_name is not None:
+            conditions.append("first_name = %s")
+            values.append(first_name)
+        if last_name is not None:
+            conditions.append("last_name = %s")
+            values.append(last_name)
+        if email is not None:
+            conditions.append("email = %s")
+            values.append(email)
+        if number is not None:
+            conditions.append("number = %s")
+            values.append(number)
+
+        if not conditions:
+            raise ValueError("Не указаны параметры для поиска")
+
+        query = f"SELECT * FROM {table_name} WHERE {' AND '.join(conditions)}"
+
+        with conn.cursor() as cur:
+            cur.execute(query, tuple(values))
+            result = cur.fetchall()
+
+        return result
+create_table()
+
 
 conn.close()
